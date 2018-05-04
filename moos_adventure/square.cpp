9a23facd216fb5f967adc3e3cb1ee7698365af6c
Nicholas Anderson <drakkor611@gmail.com>
2018-05-03 22:06:25 -0600

    trials

--- moos_adventure/square.cpp
+++ moos_adventure/square.cpp
@@ -29,4 +29,47 @@ squares::Square::Square(std::string square) {
 	_x = stoi(x[0]);
 	_y = stoi(x[1]);
 
+	for (char& c : square ) {
+		if ((c) = '1')
+		{
+		std::cout << "True ";
+		true;
+		}
+		else
+		{
+		std::cout << "false ";
+		false;
+		}
+
+		
+		SDL_Renderer* renderer = NULL;
+		SDL_bool done = SDL_FALSE;
+
+		while (!done) {
+		SDL_Event event;
+
+		SDL_SetRenderDrawColor(renderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
+		SDL_RenderClear(renderer);
+
+		SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+		SDL_RenderDrawLine(renderer, 25, 25, 75, 25);
+		//SDL_RenderDrawLine(renderer, 300, 240, 340, 240);
+		//SDL_RenderDrawLine(renderer, 340, 240, 320, 200);
+		SDL_RenderPresent(renderer);
+
+		while (SDL_PollEvent(&event)) {
+		if (event.type == SDL_QUIT) {
+		done = SDL_TRUE;
+		}
+		}
+		}
+
+		if (renderer) {
+		SDL_DestroyRenderer(renderer);
+		}
+
+		SDL_Quit();
+		//return 0;
+		
 }
+
