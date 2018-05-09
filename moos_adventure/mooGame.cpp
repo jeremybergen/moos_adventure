e0385fa5565c331f92482fba183f0f631977ab21
wstory21 <wbstory@mavs.coloradomesa.edu>
2018-05-09 00:57:13 -0600

    sync

--- moos_adventure/mooGame.cpp
+++ moos_adventure/mooGame.cpp
@@ -384,7 +384,7 @@ void games::mooGame::evalControls() {
 		//no--;
 		it = selectedLevel.squareMap.begin();
 		it2 = selectedLevel.squareMap.begin();
-		
+		std::cout << "";
 	}
 	if (failedCommands || hasGoal == false) {
 		character[0]->setPx(it->second->getX());
